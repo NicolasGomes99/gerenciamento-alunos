aa54f9dfb8a0984acb1d25c33afdda8175c5df5a
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2025-07-01 15:20:14 -0300

    feat: testes unitários dos repositories

--- src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/AlunoRepositoryTest.java
@@ -1,10 +1,58 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoRepositoryTest {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Test
+    public void deveBuscarPorStatusAtivo() {
+        Aluno a = novoAluno("Ativo", Status.ATIVO);
+        alunoRepository.save(a);
+        Assert.assertFalse(alunoRepository.findByStatusAtivo().isEmpty());
+    }
+
+    @Test
+    public void deveBuscarPorStatusInativo() {
+        Aluno a = novoAluno("Inativo", Status.INATIVO);
+        alunoRepository.save(a);
+        Assert.assertFalse(alunoRepository.findByStatusInativo().isEmpty());
+    }
+
+    @Test
+    public void deveBuscarPorNome() {
+        Aluno a = novoAluno("NomeUnico", Status.ATIVO);
+        alunoRepository.save(a);
+        Assert.assertFalse(alunoRepository.findByNomeContainingIgnoreCase("nome").isEmpty());
+    }
+
+    @Test
+    public void deveSalvarERecuperarAluno() {
+        Aluno a = novoAluno("TestePersistencia", Status.ATIVO);
+        alunoRepository.save(a);
+        Assert.assertTrue(alunoRepository.findById(a.getId()).isPresent());
+    }
+
+    private Aluno novoAluno(String nome, Status status) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(nome);
+        aluno.setMatricula("MAT-" + System.currentTimeMillis());
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(status);
+        aluno.setTurno(Turno.MATUTINO);
+        return aluno;
+    }
 }
--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,54 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.model.Usuario;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void deveSalvarUsuario() {
+        Usuario user = novoUsuario("test1@email.com", "user1", "senha");
+        usuarioRepository.save(user);
+        Assert.assertNotNull(user.getId());
+    }
+
+    @Test
+    public void deveBuscarPorEmail() {
+        Usuario user = novoUsuario("buscar@email.com", "userBuscar", "senha");
+        usuarioRepository.save(user);
+        Usuario encontrado = usuarioRepository.findByEmail("buscar@email.com");
+        Assert.assertNotNull(encontrado);
+    }
+
+    @Test
+    public void deveBuscarPorLogin() {
+        Usuario user = novoUsuario("login@email.com", "userlogin", "senha123");
+        usuarioRepository.save(user);
+        Usuario login = usuarioRepository.buscarLogin("userlogin", "senha123");
+        Assert.assertNotNull(login);
+    }
+
+    @Test
+    public void buscarLoginInvalidoRetornaNull() {
+        Usuario login = usuarioRepository.buscarLogin("fake", "fake");
+        Assert.assertNull(login);
+    }
+
+    private Usuario novoUsuario(String email, String user, String senha) {
+        Usuario u = new Usuario();
+        u.setEmail(email);
+        u.setUser(user);
+        u.setSenha(senha);
+        return u;
+    }
 }
