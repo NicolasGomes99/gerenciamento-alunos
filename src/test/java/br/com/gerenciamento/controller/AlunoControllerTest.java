5e00b93f30d9bb3eee714556b3c688a3ffb15deb
NicolasGomes99 <nicolasgabrielgomes9@gmail.com>
2025-07-01 15:20:47 -0300

    feat: testes de integração dos controllers

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,48 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveRetornarFormularioInserirAlunos() throws Exception {
+        mockMvc.perform(get("/inserirAlunos"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deveRedirecionarAposSalvar() throws Exception {
+        mockMvc.perform(post("/InsertAlunos")
+                        .param("nome", "Teste Aluno")
+                        .param("matricula", "MAT123")
+                        .param("curso", "ADMINISTRACAO")
+                        .param("status", "ATIVO")
+                        .param("turno", "NOTURNO"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void deveListarAlunos() throws Exception {
+        mockMvc.perform(get("/alunos-adicionados"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deveFiltrarPorNome() throws Exception {
+        mockMvc.perform(post("/pesquisar-aluno")
+                        .param("nome", "Teste"))
+                .andExpect(status().isOk());
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,47 @@
 package br.com.gerenciamento.controller;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveAbrirTelaLogin() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deveAbrirTelaCadastro() throws Exception {
+        mockMvc.perform(get("/cadastro"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deveCadastrarUsuario() throws Exception {
+        mockMvc.perform(post("/salvarUsuario")
+                        .param("user", "integUser")
+                        .param("email", "integ@email.com")
+                        .param("senha", "123456"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void loginInvalidoDeveRedirecionar() throws Exception {
+        mockMvc.perform(post("/login")
+                        .param("user", "naoexiste")
+                        .param("senha", "errada"))
+                .andExpect(status().isOk());
+    }
 }
